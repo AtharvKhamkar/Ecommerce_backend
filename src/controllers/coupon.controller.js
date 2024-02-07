cc196e385fe56f7832f9365fe4a9ef7c1d67bdf9
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-02-08 00:11:25 +0530

    add all coupon CRUD controllers

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Coupon } from "../models/coupon.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -21,5 +22,62 @@ const createCoupon = asyncHandler(async (req, res) => {
     )
 })
 
-export { createCoupon };
+const getAllCoupons = asyncHandler(async (req, res) => {
+    const coupons = await Coupon.find()
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                coupons,
+                "Coupons fetched successfully"
+            )
+    )
+})
+
+const updateCoupons = asyncHandler(async (req, res) => {
+    const { Id } = req.params
+    
+    const coupons = await Coupon.findByIdAndUpdate(
+        new mongoose.Types.ObjectId(Id),
+        {
+            $set: {
+                name: req.body?.name,
+                expire: req.body?.expire,
+                discount:req.body?.discount
+            }
+        },
+        {
+            new:true
+        }
+    ).select("-createdAt -updatedAt -__V")
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                coupons,
+                "Coupons updated successfully"
+        )
+    )
+})
+
+const deleteCoupon = asyncHandler(async (req, res) => {
+    const { Id } = req.params
+    
+    const coupon = await Coupon.findByIdAndDelete(Id)
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    "Deleted_coupon":coupon.name
+                },
+                "coupon deleted successfully"
+        )
+    )
+ })
+
+export { createCoupon, deleteCoupon, getAllCoupons, updateCoupons };
 
--- src/routes/coupon.routes.js
+++ src/routes/coupon.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { createCoupon, deleteCoupon, getAllCoupons, updateCoupons } from "../controllers/coupon.controller.js";
 import { isAdmin, verifyJWT } from "../middlewares/authMiddleware.js";
-import { createCoupon } from "../controllers/coupon.controller.js";
 
 const router = Router()
 
 router.route("/add-coupon").post(verifyJWT, isAdmin, createCoupon)
+router.route("/all-coupon").get(verifyJWT, getAllCoupons)
+router.route("/update-coupon/:Id").patch(verifyJWT, isAdmin, updateCoupons)
+router.route("/delete-coupon/:Id").delete(verifyJWT,isAdmin,deleteCoupon)
 
 export default router
